c199f269e7c70414d0cc7e8b8402168b09ef1bee
Carlton Joseph <carlton.joseph@gmail.com>
2024-09-08 20:25:47 -0700

    enable sh.nix in home.nix

--- sh.nix
+++ sh.nix
@@ -1,25 +1,25 @@
 { config, pkgs, ... }:
-#let 
-#  myAliases = {
-#    ll = "ls -l";
-#    la = "ls -la";
-#    n = "nvim";
-#    ".." = "cd ..";
-#    update = "sudo nixos-rebuild switch --flake .#nixos_vm";
-#  };
+let 
+  myAliases = {
+    ll = "ls -l";
+    la = "ls -la";
+    n = "nvim";
+    ".." = "cd ..";
+    update = "sudo nixos-rebuild switch --flake .#nixos_vm";
+  };
 in
 {
-#  programs = {
-#    bash = {
-#      enable = true;
-#      shellAliases = myAliases;
-#    };
-#    zsh = {
-#      enable = true;
-#      enableCompletion = true;
-#      autosuggestion.enable = true;
-#      syntaxHighlighting.enable = true;
-#      shellAliases = myAliases;
-#    };
-#  };
+  programs = {
+    bash = {
+      enable = true;
+      shellAliases = myAliases;
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = myAliases;
+    };
+  };
 }
